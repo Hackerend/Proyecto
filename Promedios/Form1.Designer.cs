e2c120c432c95910f6fe4f3b6a15f10bca44ae84
Hackerend <or23013@ues.edu.sv>
2023-09-01 17:07:02 -0600

    Agregar archivos de proyecto.

---
+++ Promedios/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Promedios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtFalta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Enabled = false;
+            this.txtEstado.Location = new System.Drawing.Point(183, 146);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(100, 22);
+            this.txtEstado.TabIndex = 1;
+            // 
+            // txtFalta
+            // 
+            this.txtFalta.Location = new System.Drawing.Point(183, 97);
+            this.txtFalta.Name = "txtFalta";
+            this.txtFalta.Size = new System.Drawing.Size(100, 22);
+            this.txtFalta.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Estado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dias Faltados";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(23, 224);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(105, 39);
+            this.btnEjecutar.TabIndex = 11;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(274, 224);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(105, 39);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(51, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Promedio Final";
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(183, 40);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 22);
+            this.txtPromedio.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 330);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFalta);
+            this.Controls.Add(this.txtEstado);
+            this.Name = "Form1";
+            this.Text = "Expediente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtFalta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPromedio;
+    }
+}
+
---
+++ Promedios/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Promedios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            int faltas,promedio,total;
+            promedio = int.Parse(txtPromedio.Text);
+            faltas = int.Parse(txtFalta.Text);
+            if (promedio > 0 && faltas >= 0)
+            {
+                if (promedio > 10)
+                {
+                    MessageBox.Show(String.Format("No puedes ingresar numeros mayores que 10", MessageBoxButtons.OK, MessageBoxIcon.Error));
+                    txtEstado.Text = "Error";
+                }
+                if (faltas >= 10)
+                {
+                    promedio = promedio - 6;
+                }
+               else if (promedio >= 6 && promedio <= 10)
+                {
+                    txtEstado.Text = "Aprovado";
+                }
+
+                if (promedio < 6)
+                {
+                    txtEstado.Text = "Reprobado";
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show(String.Format("No puedes ingresar numeros negativos", MessageBoxButtons.OK, MessageBoxIcon.Error));
+                txtEstado.Text = "Error";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
